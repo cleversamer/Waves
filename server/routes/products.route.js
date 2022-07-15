b3a2718592856e593faccd18872f50a7e8f3af11
Samer A <thedev.samer@gmail.com>
2022-07-15 10:02:49 +0300

    added a validation middleware for post prodcut route

--- server/routes/products.route.js
+++ server/routes/products.route.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const router = Router();
 const { productsController } = require("../controllers");
+const { addProductValidator } = require("../middleware/validation");
 const auth = require("../middleware/auth");
 
 router
   .route("/")
   .get(productsController.getAllProducts)
-  .post(auth("createAny", "product"), productsController.addProduct);
+  .post(
+    [auth("createAny", "product"), addProductValidator],
+    productsController.addProduct
+  );
 
 module.exports = router;
